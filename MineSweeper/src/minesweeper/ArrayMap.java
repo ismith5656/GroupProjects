c7e752c45a08a8cec2397bde58ae7d72be7a2ce5
ismith5656 <ismith5656@004LAB103001107.neisd.ad>
2016-02-26 11:26:24 -0600

    projects

--- MineSweeper/src/minesweeper/ArrayMap.java
+++ MineSweeper/src/minesweeper/ArrayMap.java
@@ -5,30 +5,61 @@
  */
 package minesweeper;
 
+import java.util.Random;
+
 /**
  *
  * @author ismith5656
  */
 public class ArrayMap {
+
     char[][] Map;
     int numBombs;
     boolean[][] bomb;
-    
-    public ArrayMap(char[][] Map, int numBombs,boolean[][] bomb,int ROW, int COL ){
+
+    public ArrayMap(char[][] Map, int numBombs, boolean[][] bomb) {
         this.Map = Map;
         this.numBombs = numBombs;
         this.bomb = bomb;
-        boolean run = true;
-        int i = 0;
-        for(int R = 0; R < ROW;R++){
-            for(int C = 0;C < COL;C++){
+    }
+
+    public void MakeMap(int ROW, int COL) {
+        for (int R = 0; R < ROW; R++) {
+            for (int C = 0; C < COL; C++) {
                 Map[ROW][COL] = '0';
-                if(i != numBombs){
-                    
-                }
+                bomb[ROW][COL] = false;
             }
         }
-                
-        
     }
+
+    public void ranBombGen(int ROW, int COL) {
+        Random r = new Random();
+        int x;
+        int y;
+        for (int i = 0; i < this.numBombs; i++) {
+            x = r.nextInt(ROW);
+            y = r.nextInt(COL);
+            this.bomb[x][y] = true;
+        }
+    }
+    public void checkClose(int x, int y){
+        for(int i = 0; i < 2; i++){
+            if(this.bomb[x][y]){
+                
+            }
+        }
+    }
+    public int up(int y){
+        return y + 1;
+    }
+public int down(int y){
+        return y - 1;
+    }
+public int right(int x){
+        return x + 1;
+    }
+public int left(int y){
+        return y - 1;
+    }
+
 }
