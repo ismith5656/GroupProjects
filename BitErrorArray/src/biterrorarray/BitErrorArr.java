922f78e13249f5596eda29f84e7950abf5a5eddf
ismith5656 <ismith5656@004LAB103001107.neisd.ad>
2016-04-06 11:26:10 -0500

    projects

--- BitErrorArray/src/biterrorarray/BitErrorArr.java
+++ BitErrorArray/src/biterrorarray/BitErrorArr.java
@@ -4,126 +4,108 @@
  * and open the template in the editor.
  */
 package biterrorarray;
+
 import java.util.Random;
+
 /**
  *
  * @author ismith5656
  */
- public class BitErrorArr {
+public class BitErrorArr {
+
     private final int ROW;
-    private final int COL;
+    private final int COL;  
     private final Random r = new Random();
     private final boolean even;
     private int[][] bitArr;
-    
-    public BitErrorArr(){
-        this.ROW = 8;  
-        this.COL = 8;  
-        this.even = r.nextBoolean() ;
-        
-    }
-    
-private int[][] makeArray(int arr[][], int ROW, int COL, boolean even){
-     arr = new int[ROW][COL];
-     even = r.nextBoolean() ;
-       if(even){
-        for (int i = 0; i < ROW;i++){
-            for( int j = 0; j < COL;j++){
-                if (i <= 7 && j <= 6){
-                    arr[i][j] = r.nextInt(2);
-                }
-                if (i <= 7 && j == 7){
-                    arr[i][j] =1;
-                }
-                
-                 
-            }
-        }
-       }else{
-         for (int i = 0; i < ROW;i++){
-            for( int j = 0; j < COL;j++){
-                if (i <= 7 && j <= 6){
-                    arr[i][j] = r.nextInt(2);
-                }
-                if (i <= 7 && j == 7){
-                    arr[i][j] =1;
-                } 
-                 
-            }
-        }
-       }
-  return arr;
-}
-public void check(int arr[][], int ROW, int COL, boolean even){
-    int[][] biarrV = new int[ROW][COL];
-    int[][] biarrH = new int[ROW][COL];
-    for(int i = 0;i < ROW;i++){
-        for(int j = 0;j < COL;j++){
-            if(i <= 0 && j==0){
-                biarrV[i][j] = 1;
-            }else{
-                if(i <= 1 && j==1){
-                biarrV[i][j] = 2;
-                }else{
-                    biarrV[i][j] = biarrV[i-1][j]^2;
-                }
-            }
-            
-            if(i == 0 && j<=0){
-                biarrH[i][j] = 1;
-            }else{
-                if(i == 1 && j<=1){
-                biarrH[i][j] = 2;
-                }else{
-                    biarrH[i][j] = biarrH[i][j-1]^2;
-                }
-            }
-        }
-    }
-    if(even){
-        
-    }else{
-        
-    }
-    for(int i = 0;i < ROW;i++){
-        for(int j = 0;j < COL;j++){
-           if(j == 0){
-                    System.out.print( i +"|");
-                }
-                if(j < 7){
-                    System.out.print(biarrV[i][j] + " ");
-                }else{
-                    System.out.print(biarrV[i][j]);
-                }
-                
-                if( j == 7 ){
-                    System.out.print("");
-                    System.out.println("");
-                }
-        }
-    }
-    for(int i = 0;i < ROW;i++){
-        for(int j = 0;j < COL;j++){
-           if(j == 0){
-                    System.out.print( i +"|");
-                }
-                if(j < 7){
-                    System.out.print(biarrH[i][j] + " ");
-                }else{
-                    System.out.print(biarrH[i][j]);
-                }
-                
-                if( j == 7 ){
-                    System.out.print("");
-                    System.out.println("");
-                }
-        }
-    }
-}
-public int[][] createArr(){
 
-return makeArray(this.bitArr, this.ROW, this.COL, this.even);
-    
-}
+    public BitErrorArr() {
+        this.ROW = 8;
+        this.COL = 8;
+        this.even = r.nextBoolean();
+
+    }
+
+    private int[][] makeArray(int arr[][], int ROW, int COL, boolean even) {
+        arr = new int[ROW][COL];
+        even = r.nextBoolean();
+        if (even) {
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
+                    if (i <= 7 && j <= 6) {
+                        arr[i][j] = r.nextInt(2);
+                    }
+                    if (i <= 7 && j == 7) {
+                        arr[i][j] = 1;
+                    }
+
+                }
+            }
+        } else {
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
+                    if (i <= 7 && j <= 6) {
+                        arr[i][j] = r.nextInt(2);
+                    }
+                    if (i <= 7 && j == 7) {
+                        arr[i][j] = 1;
+                    }
+
+                }
+            }
+        }
+        return arr;
+    }
+
+    public void checkCol(int arr[][], int ROW, int COL, boolean even) {
+
+        int[][] biarrH = new int[ROW][COL];
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                
+
+                if (i == 0 && j < 8) {
+                    biarrH[i][j] = 1;
+                } else {
+                    if (i == 1 && j < 8) {
+                        biarrH[i][j] = 2;
+                    } else {
+                        biarrH[i][j] = biarrH[i-1][j] * 2;
+                    }
+                }
+            }
+        }
+        if (even) {
+
+        } else {
+
+        }
+       
+       
+    }
+
+    public void checkRow(int arr[][], int ROW, int COL, boolean even) {
+        int[][] biarrV = new int[ROW][COL];
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (i < ROW && j == 0) {
+                    biarrV[i][j] = 1;
+                } else {
+                    if (i < ROW && j == 1) {
+                        biarrV[i][j] = 2;
+                    } else {
+                        biarrV[i][j] = biarrV[i][j-1] * 2;
+                    }
+                }
+            }
+        }
+         
+    }
+
+    public int[][] createArr() {
+
+        return makeArray(this.bitArr, this.ROW, this.COL, this.even);
+
+    }
 
 }
--- BitErrorArray/src/biterrorarray/BitErrorArray.java
+++ BitErrorArray/src/biterrorarray/BitErrorArray.java
@@ -20,25 +20,57 @@ public class BitErrorArray {
         // TODO code application logic here
         BitErrorArr arr = new BitErrorArr();
         int[][] Arr = arr.createArr();
-        
-        printArr(Arr);
+
+        //printArr(Arr);
+         int[][] biarrV = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i == 0 && j < 8) {
+                    biarrV[i][j] = 1;
+                } else {
+                    if (i == 1 && j < 8) {
+                        biarrV[i][j] = 2;
+                    } else {
+                        biarrV[i][j] = biarrV[i-1][j]*2;
+                    }
+                }
+            }
+        }
+         for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 0) {
+                    System.out.print(i + "|");
+                }
+                if (j < 7) {
+                    System.out.print(biarrV[i][j] + " ");
+                } else {
+                    System.out.print(biarrV[i][j]);
+                }
+
+                if (j == 7) {
+                    System.out.print("");
+                    System.out.println("");
+                }
+            }
+        }
         
     }
-    public static void printArr(int Arr[][]){
+
+    public static void printArr(int Arr[][]) {
         System.out.println("  1 2 3 4 5 6 7 8");
         System.out.println(" _________________");
-        for (int i = 0; i < 8;i++){
-            for( int j = 0; j < 8;j++){
-                if(j == 0){
-                    System.out.print( i +"|");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 0) {
+                    System.out.print(i + "|");
                 }
-                if(j < 7){
+                if (j < 7) {
                     System.out.print(Arr[i][j] + " ");
-                }else{
+                } else {
                     System.out.print(Arr[i][j]);
                 }
-                
-                if( j == 7 ){
+
+                if (j == 7) {
                     System.out.print("");
                     System.out.println("");
                 }
@@ -46,5 +78,4 @@ public class BitErrorArray {
         }
     }
 
-    
 }
