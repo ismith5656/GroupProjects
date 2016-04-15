884dd3e320b901c74bfc27e34a657fc842ce0fa5
ismith5656 <ismith5656@004LAB103001107.neisd.ad>
2016-04-15 11:27:39 -0500

    projects

--- VendingMachine/src/vendingmachine/VendingMachine.java
+++ VendingMachine/src/vendingmachine/VendingMachine.java
@@ -7,6 +7,8 @@ package vendingmachine;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -19,37 +21,50 @@ public class VendingMachine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<List<List<Object>>> create = create(3,3,3);
-       
-       
-    
-}
- public static <E> List<List<List<E>>> create(int dim1, int dim2, int dim3)
-{
-    List<List<List<E>>> list1 = new ArrayList<List<List<E>>>(dim1);
-    for (int i = 0; i < dim1; i++)
-    {
-        List<List<E>> list2 = new ArrayList<List<E>>(dim2);
-        for (int j = 0; j < dim2; j++)
-        {
-            List<E> list3 = new ArrayList<E>(dim3);
-            for (int k = 0; k < dim3; k++)
-            {
-                list3.add(null);
+        Scanner input = new Scanner(System.in);
+        String in;
+        List<List<List<Object>>> create = create(3, 3, 3);
+        in = input.next();
+        System.out.println(checkLetter(in));
+    }
+
+    public static <E> List<List<List<E>>> create(int dim1, int dim2, int dim3) {
+        List<List<List<E>>> list1 = new ArrayList<List<List<E>>>(dim1);
+        for (int i = 0; i < dim1; i++) {
+            List<List<E>> list2 = new ArrayList<List<E>>(dim2);
+            for (int j = 0; j < dim2; j++) {
+                List<E> list3 = new ArrayList<E>(dim3);
+                for (int k = 0; k < dim3; k++) {
+                    list3.add(null);
+                }
+                list2.add(list3);
             }
-            list2.add(list3);
+            list1.add(list2);
         }
-        list1.add(list2);
+        return list1;
     }
-    return list1;
+
+    public static int checkLetter(String input) {
+        String exp1 = "[a-c]";
+        String exp2 = "[A-C]";
+        int test;
+        CharSequence inputStr = input;
+        Pattern correctLetter = Pattern.compile(exp1);
+        Pattern correctLetterCap = Pattern.compile(exp2);
+        Matcher check = correctLetter.matcher(inputStr);
+        Matcher checkCap = correctLetterCap.matcher(inputStr);
+        if (check.matches()) {
+            test = (int) input.charAt(0) - 97;
+        }else{
+            test = 100;
+        }
+        if(checkCap.matches()){
+            test = (int) input.charAt(0) - 65;
+        }else{
+            test = 100;
+        }
+
+        return test;
+
+    }
 }
- public int checkLetter(String input){
-     String exp1 = "[a-cA-C]";
-     String exp2= "[d-zD-Z]";
-     CharSequence inputStr = input;
-     Pattern correctLetter = Pattern.compile(exp1);
-     Pattern incorrectLetter = Pattern.compile(exp2);
-    int test = (int)'b'- 96;
-    return test;
- }
-    }
